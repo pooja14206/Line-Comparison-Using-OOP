ce199a02192972334c618278a2da6f447e1a4faa
Pooja <pooja.14206@gmail.com>
2021-04-13 04:33:33 +0530

    [POOJA] added program of checking the equality of two length

--- src/lineComparison/Line.java
+++ src/lineComparison/Line.java
@@ -3,15 +3,36 @@ package lineComparison;
 public class Line {
 	Point p1;
 	Point p2;
-	
-	public Line(Point p1, Point p2)
-	{
+	Double length;
+
+	public Line(Point p1, Point p2) {
 		this.p1 = p1;
 		this.p2 = p2;
 	}
-	double getLength() 
-	{
-		return Math.sqrt((p2.x - p1.x) *(p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+
+	double getLength() {
+		double length;
+		length = Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+		this.length = length;
+		return length;
 	}
 
+	@Override
+	public boolean equals(Object length2) {
+		if (this == length2)
+			return true;
+		if (length2 == null)
+			return false;
+		if (getClass() != length2.getClass())
+			return false;
+
+		Line other = (Line) length2;
+		if (other.length == null)
+			return false;
+		if (this.length == null)
+			return false;
+		if (!this.length.equals(other.length))
+			return false;
+		return true;
+	}
 }
