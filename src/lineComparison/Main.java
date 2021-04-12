dfff6e38696c28cb1808b7e92cecae30fd8ae453
Pooja <pooja.14206@gmail.com>
2021-04-13 04:42:57 +0530

    [POOJA] compare 2 lengths

--- src/lineComparison/Main.java
+++ src/lineComparison/Main.java
@@ -2,10 +2,10 @@ package lineComparison;
 
 public class Main {
 	public static void main(String[] args) {
-		Point p1 = new Point(10, 15);
+		Point p1 = new Point(10, 5);
 		Point p2 = new Point(0, -3);
 		Point p3 = new Point(10, 7);
-		Point p4 = new Point(9, -3);
+		Point p4 = new Point(9, -13);
 
 		Line l1 = new Line(p1, p2);
 		Line l2 = new Line(p3, p4);
@@ -15,8 +15,10 @@ public class Main {
 
 		if (l1.equals(l2))
 			System.out.println("Equal");
+		else if (l1.compare(l2) > 0)
+			System.out.println("Frist line is greater.");
 		else
-			System.out.println("NotEqual");
+			System.out.println("First line is smaller");
 
 	}
 
